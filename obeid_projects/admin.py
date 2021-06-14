3c7f4d85d8ad91f471d38ac293a0124478971c62
Obeid Arash <Obeid75arash@gmail.com>
2021-06-15 02:59:33 +0430

    add more models in projects app

--- obeid_projects/admin.py
+++ obeid_projects/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import Project
+from .models import Project, Customer
 
 
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
-    list_display = ('title',)
+    list_display = ('title', 'customer')
+
+
+@admin.register(Customer)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'lastname', 'email')
--- obeid_projects/models.py
+++ obeid_projects/models.py
@@ -1,13 +1,33 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator, EmailValidator
+from django.contrib.auth.models import User
 
 
 class ProjectManager(models.Manager):
     pass
 
 
+class Customer(models.Model):
+    name = models.CharField(max_length=64, null=True, blank=True)
+    lastname = models.CharField(max_length=64, null=True, blank=True)
+    email = models.EmailField(null=True, blank=True, validators=[EmailValidator])
+    phone = models.CharField(max_length=32, blank=True, null=True)
+
+    def __str__(self):
+        return "{} {}".format(self.name, self.lastname)
+
+
 class Project(models.Model):
     title = models.CharField(max_length=256)
+    slug = models.SlugField(max_length=256, unique=True, blank=True)
+    progress = models.PositiveIntegerField(default=5, validators=[MinValueValidator(0), MaxValueValidator(100)])
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
     objects = ProjectManager()
 
     def __str__(self):
         return self.title
+
+
+class Category(models.Model):
+    pass
