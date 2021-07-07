95ebb8038533feff364778d6667b32887821f552
Obeid Arash <Obeid75arash@gmail.com>
2021-07-08 02:35:51 +0430

    add funtion to home page (completed and latest projects)

--- obeid/views.py
+++ obeid/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from obeid_projects.models import Project
 
 
 def home(request):
+    latest_projects: Project = Project.objects.filter(publish=True, progress__range=[0, 99]).order_by('-create')[:5]
+    completed_projects: Project = Project.objects.filter(progress=100, publish=True).order_by('-update')[:5]
     context = {
-        'title': 'Home'
+        'title': 'Home',
+        'latest_projects': latest_projects,
+        'completed_projects': completed_projects
     }
     return render(request, 'home.html', context)
--- obeid_tools/views.py
+++ obeid_tools/views.py
@@ -4,8 +4,10 @@ from .forms import YoutubeForm
 from pytube import YouTube
 import requests
 import pafy
+from django.contrib.auth.decorators import login_required
 
 
+@login_required(login_url='home')
 def youtube(request):
     video = ''
     youtube_form = YoutubeForm(request.POST or None)
